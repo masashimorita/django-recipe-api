9d37a27515e4443f861346e21f8727e8af4cfe1a
masashimorita <masashi.morita.mm@gmail.com>
2022-01-24 19:33:40 +0900

    feat: :+1:  Added working list tags endpoint

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,5 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@example.com', password='password', name='Test'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password,
+        name=name
+    )
 
 
 class ModelTest(TestCase):
@@ -37,3 +47,11 @@ class ModelTest(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
