d1b818b1a3701032fbac8ae3fdaa5f36d8a1d78b
masashimorita <masashi.morita.mm@gmail.com>
2023-08-13 16:33:24 +0900

    :+1: Add endpoint to update Tag

--- src/recipe/views.py
+++ src/recipe/views.py
@@ -38,7 +38,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """view for manage tags APIs"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
